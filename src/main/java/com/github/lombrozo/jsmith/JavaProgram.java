fa1086a99330e91847b2f605e290d0622824917a
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-28 18:41:25 +0300

    feat(#2): add dummy class

--- src/main/java/com/github/lombrozo/jsmith/JavaProgram.java
+++ src/main/java/com/github/lombrozo/jsmith/JavaProgram.java
@@ -1,21 +1,29 @@
 package com.github.lombrozo.jsmith;
 
-public class JavaProgram {
+public final class JavaProgram {
 
-    private final String src;
+    private final String source;
+
+    public JavaProgram() {
+        this(JavaProgram.helloWorld());
+    }
 
     public JavaProgram(final String src) {
-        this.src = src;
+        this.source = src;
     }
 
-    public String sourceCode() {
-        return src;
+    public String src() {
+        return this.source;
     }
 
-    private final String helloWorld(){
+    private static String helloWorld() {
         return String.join(
             "\n",
-            ""
+            "public class HelloWorld {",
+            "    public static void main(String[] args) {",
+            "        System.out.println(\"Hello, World!\");",
+            "    }",
+            "}"
         );
     }
 
