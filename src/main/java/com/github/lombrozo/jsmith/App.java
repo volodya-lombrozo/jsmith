ccae1c8593d49e610bf96a1bd36de49d5d1e95da
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-28 18:58:58 +0300

    feat(#2): remove the puzzle for the 2 issue

--- src/main/java/com/github/lombrozo/jsmith/App.java
+++ src/main/java/com/github/lombrozo/jsmith/App.java
@@ -1,15 +1,7 @@
 package com.github.lombrozo.jsmith;
 
-/**
- * Hello world!
- * @todo #1:90min Create Library Skeleton.
- *  Currently it's just an empty project, we have to create a simple skeleton
- *  and then publish the first version into maven central.
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
