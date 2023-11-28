91e33f4b2e95101db5a071cdc9269562ce20ce6e
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-11-28 18:11:14 +0300

    chore: add the first puzzle

--- src/main/java/com/github/lombrozo/jsmith/App.java
+++ src/main/java/com/github/lombrozo/jsmith/App.java
@@ -2,7 +2,9 @@ package com.github.lombrozo.jsmith;
 
 /**
  * Hello world!
- *
+ * @todo #1:90min Create Library Skeleton.
+ *  Currently it's just an empty project, we have to create a simple skeleton
+ *  and then publish the first version into maven central.
  */
 public class App 
 {
