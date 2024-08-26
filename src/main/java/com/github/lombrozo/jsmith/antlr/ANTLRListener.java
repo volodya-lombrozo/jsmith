c6a4aa69683d9be57ae90c969c6b80f26dae517a
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-08-26 18:24:37 +0300

    feat(antlr): add more rules

--- src/main/java/com/github/lombrozo/jsmith/antlr/ANTLRListener.java
+++ src/main/java/com/github/lombrozo/jsmith/antlr/ANTLRListener.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023-2024 Volodya Lombrozo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.lombrozo.jsmith.antlr;
 
 import com.github.lombrozo.jsmith.ANTLRv4Parser;
@@ -7,10 +30,13 @@ import com.github.lombrozo.jsmith.antlr.rules.ActionBlock;
 import com.github.lombrozo.jsmith.antlr.rules.ActionScopeName;
 import com.github.lombrozo.jsmith.antlr.rules.AltList;
 import com.github.lombrozo.jsmith.antlr.rules.Alternative;
+import com.github.lombrozo.jsmith.antlr.rules.ArgActionBlock;
 import com.github.lombrozo.jsmith.antlr.rules.Atom;
 import com.github.lombrozo.jsmith.antlr.rules.Block;
 import com.github.lombrozo.jsmith.antlr.rules.BlockSuffix;
 import com.github.lombrozo.jsmith.antlr.rules.CharacterRange;
+import com.github.lombrozo.jsmith.antlr.rules.DelegateGrammar;
+import com.github.lombrozo.jsmith.antlr.rules.DelegateGrammars;
 import com.github.lombrozo.jsmith.antlr.rules.Ebnf;
 import com.github.lombrozo.jsmith.antlr.rules.EbnfSuffix;
 import com.github.lombrozo.jsmith.antlr.rules.Element;
@@ -530,4 +556,47 @@ public final class ANTLRListener extends ANTLRv4ParserBaseListener {
         this.current = this.current.parent();
         super.exitActionScopeName(ctx);
     }
+
+    @Override
+    public void enterArgActionBlock(final ANTLRv4Parser.ArgActionBlockContext ctx) {
+        final RuleDefinition rule = new ArgActionBlock(this.current);
+        this.current.append(rule);
+        this.current = rule;
+        super.enterArgActionBlock(ctx);
+    }
+
+    @Override
+    public void exitArgActionBlock(final ANTLRv4Parser.ArgActionBlockContext ctx) {
+        this.current = this.current.parent();
+        super.exitArgActionBlock(ctx);
+    }
+
+    @Override
+    public void enterDelegateGrammar(final ANTLRv4Parser.DelegateGrammarContext ctx) {
+        final RuleDefinition rule = new DelegateGrammar(this.current);
+        this.current.append(rule);
+        this.current = rule;
+        super.enterDelegateGrammar(ctx);
+    }
+
+    @Override
+    public void exitDelegateGrammar(final ANTLRv4Parser.DelegateGrammarContext ctx) {
+        this.current = this.current.parent();
+        super.exitDelegateGrammar(ctx);
+    }
+
+    @Override
+    public void enterDelegateGrammars(final ANTLRv4Parser.DelegateGrammarsContext ctx) {
+        final RuleDefinition rule = new DelegateGrammars(this.current);
+        this.current.append(rule);
+        this.current = rule;
+        super.enterDelegateGrammars(ctx);
+    }
+
+    @Override
+    public void exitDelegateGrammars(final ANTLRv4Parser.DelegateGrammarsContext ctx) {
+        this.current = this.current.parent();
+        super.exitDelegateGrammars(ctx);
+    }
+
 }
