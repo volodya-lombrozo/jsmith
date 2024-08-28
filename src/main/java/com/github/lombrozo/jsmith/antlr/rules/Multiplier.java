6615d01c148e2bf488ac365ae7bc317184f5af29
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-08-28 10:08:39 +0300

    feat(antlr): add tests for multipliers

--- src/main/java/com/github/lombrozo/jsmith/antlr/rules/Multiplier.java
+++ src/main/java/com/github/lombrozo/jsmith/antlr/rules/Multiplier.java
@@ -1,12 +1,52 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023-2024 Volodya Lombrozo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.lombrozo.jsmith.antlr.rules;
 
 import com.github.lombrozo.jsmith.Rand;
 import java.util.Collections;
 
+/**
+ * Multiplier of rules.
+ * For example, the rule {@code a+} can be represented as
+ * {@code new OneOrMore(new Rand(), new LexerAtom("a"))}.
+ * The idea is to generate a random number of repetitions of the rule based on EBNF suffix
+ * like {@code +}, {@code *}, {@code ?}.
+ * @since 0.1
+ */
 public interface Multiplier {
 
+    /**
+     * Generate a string based on the rule and the multiplier.
+     * @param rule Rule to repeat.
+     * @return Generated string by the repetition.
+     */
     String generate(final RuleDefinition rule);
 
+    /**
+     * Exactly one repetition.
+     * @since 0.1
+     */
     final class One implements Multiplier {
 
         @Override
@@ -15,9 +55,17 @@ public interface Multiplier {
         }
     }
 
+    /**
+     * Zero or one repetition.
+     * @since 0.1
+     */
     final class ZeroOrOne implements Multiplier {
 
         private final Rand rand;
+
+        public ZeroOrOne() {
+            this(new Rand());
+        }
 
         public ZeroOrOne(final Rand rand) {
             this.rand = rand;
@@ -35,15 +83,42 @@ public interface Multiplier {
         }
     }
 
+    /**
+     * One or more repetitions.
+     * @since 0.1
+     */
     final class OneOrMore implements Multiplier {
 
+        /**
+         * Random generator.
+         */
         private final Rand rand;
+
+        /**
+         * Limit of repetitions.
+         */
         private final int limit;
 
+        /**
+         * Constructor.
+         */
+        public OneOrMore() {
+            this(new Rand());
+        }
+
+        /**
+         * Constructor.
+         * @param rand Random generator.
+         */
         public OneOrMore(final Rand rand) {
             this(rand, 5);
         }
 
+        /**
+         * Constructor.
+         * @param rand Random generator.
+         * @param limit Limit of repetitions.
+         */
         public OneOrMore(final Rand rand, final int limit) {
             this.rand = rand;
             this.limit = limit;
@@ -57,15 +132,42 @@ public interface Multiplier {
         }
     }
 
+    /**
+     * Zero or more repetitions.
+     * @since 0.1
+     */
     final class ZeroOrMore implements Multiplier {
 
+        /**
+         * Random generator.
+         */
         private final Rand rand;
+
+        /**
+         * Limit of repetitions.
+         */
         private final int limit;
 
+        /**
+         * Constructor.
+         */
+        public ZeroOrMore() {
+            this(new Rand());
+        }
+
+        /**
+         * Constructor.
+         * @param rand Random generator.
+         */
         public ZeroOrMore(final Rand rand) {
             this(rand, 5);
         }
 
+        /**
+         * Constructor.
+         * @param rand Random generator.
+         * @param limit Limit of repetitions.
+         */
         public ZeroOrMore(final Rand rand, final int limit) {
             this.rand = rand;
             this.limit = limit;
---
+++ src/test/java/com/github/lombrozo/jsmith/antlr/rules/MultiplierTest.java
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023-2024 Volodya Lombrozo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.lombrozo.jsmith.antlr.rules;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test cases for {@link Multiplier}.
+ * @since 0.1
+ */
+final class MultiplierTest {
+
+    @Test
+    void generatesZeroOrMore() {
+        MatcherAssert.assertThat(
+            "We expect that the 'zero or more' multiplier will generate zero or more elements",
+            new Multiplier.ZeroOrMore().generate(new Literal("a")),
+            Matchers.anyOf(
+                Matchers.emptyString(),
+                Matchers.equalTo("a"),
+                Matchers.containsString("a a")
+            )
+        );
+    }
+
+    @Test
+    void generatesOneOrMore() {
+        MatcherAssert.assertThat(
+            "We expect that the 'one or more' multiplier will generate one or more elements",
+            new Multiplier.OneOrMore().generate(new Literal("a")),
+            Matchers.anyOf(
+                Matchers.equalTo("a"),
+                Matchers.containsString("a a")
+            )
+        );
+    }
+
+    @Test
+    void generatesZeroOrOne() {
+        MatcherAssert.assertThat(
+            "We expect that the 'zero or one' multiplier will generate zero or one element",
+            new Multiplier.ZeroOrOne().generate(new Literal("a")),
+            Matchers.anyOf(
+                Matchers.emptyString(),
+                Matchers.equalTo("a")
+            )
+        );
+    }
+
+    @Test
+    void generatesExactlyOne() {
+        MatcherAssert.assertThat(
+            "We expect that the 'exactly one' multiplier will generate exactly one element",
+            new Multiplier.One().generate(new Literal("a")),
+            Matchers.equalTo("a")
+        );
+    }
+
+}
