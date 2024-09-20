a0531b178de068df57ddd1b92443094ff28e92b2
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-09-20 18:24:38 +0300

    feat(antlr): refactor TextTree

--- src/main/java/com/github/lombrozo/jsmith/antlr/view/TextTree.java
+++ src/main/java/com/github/lombrozo/jsmith/antlr/view/TextTree.java
@@ -29,6 +29,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -68,75 +69,147 @@ public final class TextTree implements Text {
     @Override
     public String output() {
         final Table table = new Table(TextTree.width(this.original));
-        this.travers(table, this.original, 0, 0);
+        this.fill(table, this.original, 0, 0);
         return table.toString();
     }
 
-    private static int width(final Text text) {
-        if (text.children().isEmpty()) {
-            return 1;
+    /**
+     * Calculate the width of the subtree.
+     * Example:
+     *         a
+     *       /  \
+     *      b    c
+     *    /  \  / \
+     *    d  e f   g
+     * Width of the subtree is 4.
+     * @param subtree Subtree.
+     * @return Width of the subtree.
+     */
+    private static int width(final Text subtree) {
+        final int result;
+        if (subtree.children().isEmpty()) {
+            result = 1;
+        } else {
+            result = subtree.children().stream().mapToInt(TextTree::width).sum();
         }
-        return text.children().stream().mapToInt(TextTree::width).sum();
+        return result;
     }
 
-    private void travers(final Table table, final Text current, final int x, final int y) {
+    /**
+     * Fill the table with the subtree.
+     * @param table Table to fill.
+     * @param current Current node.
+     * @param column X coordinate.
+     * @param row Y coordinate.
+     */
+    private void fill(final Table table, final Text current, final int column, final int row) {
         if (current.children().isEmpty()) {
-            table.put(new Cell(y, x, current.writer().name()));
-            table.put(new Result(y, current.output()));
+            table.put(new Cell(row, column, current.writer().name()));
+            table.put(new Output(row, current.output()));
         } else {
-            final Cell cell = new Cell(y, x, current.writer().name());
+            final Cell cell = new Cell(row, column, current.writer().name());
             table.put(cell);
             final List<Text> children = current.children();
             final int size = children.size();
             int prev = 0;
             for (int index = 0; index < size; ++index) {
                 final Text child = children.get(index);
-                this.travers(table, child, x + 1, y + prev);
+                this.fill(table, child, column + 1, row + prev);
                 prev += TextTree.width(child);
             }
         }
     }
 
+    /**
+     * Table.
+     * @since 0.1
+     */
     private final class Table {
 
+        /**
+         * All table cells.
+         */
         private final List<Cell> cells;
-        private final List<Result> results;
 
-        public Table(int size) {
-            this.cells = new ArrayList<>(0);
-            this.results = new ArrayList<>(Collections.nCopies(size, new Result(0, "")));
+        /**
+         * All rules outputs.
+         */
+        private final List<Output> outputs;
+
+        /**
+         * Constructor.
+         * @param size Number of rows.
+         */
+        private Table(int size) {
+            this(
+                new ArrayList<>(0),
+                new ArrayList<>(Collections.nCopies(size, new Output(0, "")))
+            );
         }
 
-        public void put(final Cell writer) {
-            this.cells.add(writer);
+        /**
+         * Constructor.
+         * @param cells Cells.
+         * @param outputs Outputs.
+         */
+        private Table(final List<Cell> cells, final List<Output> outputs) {
+            this.cells = cells;
+            this.outputs = outputs;
         }
 
-        public void put(final Result result) {
-            this.results.set(result.row, result);
+        /**
+         * Put the cell.
+         * @param cell Cell.
+         */
+        public void put(final Cell cell) {
+            this.cells.add(cell);
+        }
+
+        /**
+         * Put the output.
+         * @param output Output.
+         */
+        public void put(final Output output) {
+            this.outputs.set(output.row, output);
         }
 
         @Override
         public String toString() {
-            final int rows = this.numRows() + 1;
-            final int columns = this.numColumns() + 1;
+            final int rows = this.rows() + 1;
+            final int columns = this.columns() + 1;
             final String[][] map = new String[rows][columns];
-            for (int i = 0; i < rows; ++i) {
-                for (int j = 0; j < columns; ++j) {
-                    map[i][j] = Stream.generate(() -> "-")
-                        .limit(this.columnWidth(j))
-                        .collect(Collectors.joining());
-                }
-            }
-            for (final Cell cell : this.cells) {
-                final int width = this.columnWidth(cell.column());
-                map[cell.row()][cell.column()] = cell.pretty(width);
-            }
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[i].length; j++) {
-                    final int length = map[i][j].length();
-                    if (map[i][j].replace("-", "").isEmpty()) {
+            this.fillByDashes(map);
+            this.fillByCellValues(map);
+            this.clearLeftCells(map);
+            return String.join("\n", this.toLines(map));
+        }
 
-                        map[i][j] = Stream.generate(() -> " ")
+        /**
+         * Convert the table to the list of lines.
+         * @param map Table.
+         * @return List of lines.
+         */
+        private List<String> toLines(final String[][] map) {
+            return IntStream.range(0, map.length)
+                .mapToObj(
+                    index -> String.format(
+                        "%s --> %s",
+                        String.join(" ", map[index]),
+                        this.outputs.get(index).text()
+                    )
+                ).collect(Collectors.toList());
+        }
+
+        /**
+         * Clear left cells before the first cell with the text.
+         * @param map Table.
+         */
+        private void clearLeftCells(final String[][] map) {
+            for (int row = 0; row < map.length; row++) {
+                for (int column = 0; column < map[row].length; column++) {
+                    final int length = map[row][column].length();
+                    if (map[row][column].replace("-", "").isEmpty()) {
+                        map[row][column] = Stream.generate(() -> " ")
                             .limit(length - 2)
                             .collect(Collectors.joining("", " ", "|"));
                     } else {
@@ -144,65 +217,133 @@ public final class TextTree implements Text {
                     }
                 }
             }
-            final List<String> lines = new ArrayList<>(0);
-            for (int i = 0; i < rows; ++i) {
-                lines.add(
-                    Stream.concat(
-                        Arrays.stream(map[i]),
-                        Stream.of(String.format("--> %s", this.results.get(i).text()))
-                    ).collect(Collectors.joining(" "))
-                );
-            }
-            return String.join("\n", lines);
         }
 
-        private int numRows() {
+        /**
+         * Fill the table by cell values.
+         * @param map Table.
+         */
+        private void fillByCellValues(final String[][] map) {
+            for (final Cell cell : this.cells) {
+                map[cell.row()][cell.column()] = cell.pretty(this.width(cell.column()));
+            }
+        }
+
+        /**
+         * Fill the table cells by dashes.
+         * @param map Table.
+         */
+        private void fillByDashes(final String[][] map) {
+            for (int row = 0; row < map.length; ++row) {
+                for (int column = 0; column < map[row].length; ++column) {
+                    map[row][column] = Stream.generate(() -> "-")
+                        .limit(this.width(column))
+                        .collect(Collectors.joining());
+                }
+            }
+        }
+
+        /**
+         * Number of rows.
+         * @return Number of rows.
+         */
+        private int rows() {
             return this.cells.stream()
                 .mapToInt(Cell::row)
                 .max()
                 .orElse(0);
         }
 
-        private int numColumns() {
+        /**
+         * Number of columns.
+         * @return Number of columns.
+         */
+        private int columns() {
             return this.cells.stream()
                 .mapToInt(Cell::column)
                 .max()
                 .orElse(0);
         }
 
-        private int columnWidth(final int column) {
+        /**
+         * Max column width.
+         * Calculates the max length of the cell in the column.
+         * @param column Column.
+         * @return Max column width.
+         */
+        private int width(final int column) {
             return this.cells.stream()
                 .filter(cell -> cell.column() == column)
                 .mapToInt(Cell::length)
                 .max()
                 .orElse(0);
         }
-
-
     }
 
-    private static final class Result {
+    /**
+     * Final output of some branch.
+     * {@link Output} always is the last element in the row.
+     * It's why we need to store the row number
+     * and don't need to store the column number.
+     * @since 0.1
+     */
+    private static final class Output {
+        /**
+         * Row.
+         */
         private final int row;
+
+        /**
+         * Text.
+         */
         private final String text;
 
-        public Result(final int row, final String text) {
+        /**
+         * Constructor.
+         * @param row Row.
+         * @param text Text.
+         */
+        private Output(final int row, final String text) {
             this.row = row;
             this.text = text;
         }
 
-        public String text() {
+        /**
+         * Get the resulting text.
+         * @return Text.
+         */
+        String text() {
             return this.text;
         }
     }
 
+    /**
+     * Cell in the table.
+     */
     private static final class Cell {
 
+        /**
+         * X coordinate.
+         */
         private final int row;
+
+        /**
+         * Y coordinate.
+         */
         private final int column;
 
+        /**
+         * Text.
+         */
         private final String text;
 
-        public Cell(
+        /**
+         * Constructor.
+         * @param row Row.
+         * @param column Column.
+         * @param text Text.
+         */
+        Cell(
             final int row,
             final int column,
             final String text
@@ -212,24 +353,42 @@ public final class TextTree implements Text {
             this.text = text;
         }
 
-        public int row() {
+        /**
+         * Get the row.
+         * @return Row.
+         */
+        int row() {
             return this.row;
         }
 
-        public int column() {
+        /**
+         * Get the column.
+         * @return Column.
+         */
+        int column() {
             return this.column;
         }
 
-        public int length() {
+        /**
+         * Get the text length.
+         * @return Text length.
+         */
+        int length() {
             return this.text.length();
         }
 
-        public String pretty(final int maxLength) {
+        /**
+         * Pretty print.
+         * If the text is shorter than the column width, fill the rest with '-'.
+         * @param max column with.
+         * @return Pretty text.
+         */
+        String pretty(final int max) {
             return String.format(
                 "%s%s",
                 this.text,
                 Stream.generate(() -> "-")
-                    .limit(maxLength - this.text.length())
+                    .limit(max - this.text.length())
                     .collect(Collectors.joining())
             );
         }
