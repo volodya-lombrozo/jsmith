af8320b3fe8683c7b181e5d9de6d212b03b79f2d
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-10-30 15:47:12 +0300

    feat(antlr): fix DotTreeTest

--- src/test/java/com/github/lombrozo/jsmith/antlr/view/DotTreeTest.java
+++ src/test/java/com/github/lombrozo/jsmith/antlr/view/DotTreeTest.java
@@ -39,58 +39,40 @@ final class DotTreeTest {
 
     @Test
     void convertsSimpleTree() {
-        final String output = new DotTree(
-            new TextNode(
-                new Root(),
-                Arrays.asList(
-                    new TextLeaf(new Literal("a"), "a"),
-                    new TextLeaf(new Literal("b"), "b"),
-                    new TextNode(
-                        new Literal("c"),
-                        Arrays.asList(
-                            new TextLeaf(new Literal("d"), "d"),
-                            new TextLeaf(new Literal("e"), "e"),
-                            new TextLeaf(new Literal("a"), "a")
-                        )
-                    ),
-                    new TextNode(
-                        new Literal("f"),
-                        Collections.singletonList(
-                            new TextLeaf(new Literal("d"), "d")
+        MatcherAssert.assertThat(
+            "We expect that the tree will be converted to DOT format",
+            new DotTree(
+                new TextNode(
+                    new Root(),
+                    Arrays.asList(
+                        new TextLeaf(new Literal("a"), "a"),
+                        new TextLeaf(new Literal("b"), "b"),
+                        new TextNode(
+                            new Literal("c"),
+                            Arrays.asList(
+                                new TextLeaf(new Literal("d"), "d"),
+                                new TextLeaf(new Literal("e"), "e"),
+                                new TextLeaf(new Literal("a"), "a")
+                            )
+                        ),
+                        new TextNode(
+                            new Literal("f"),
+                            Collections.singletonList(
+                                new TextLeaf(new Literal("d"), "d")
+                            )
                         )
                     )
                 )
-            )
-        ).output();
-        System.out.println(output);
-        MatcherAssert.assertThat(
-            output,
-            Matchers.equalTo(
-                String.join(
-                    "\n",
-                    "digraph JsmithGenerativeTree{",
-                    "\"#1558021762\" -> \"#225290371\";",
-                    "\"#225290371\" -> \"#733672688\";",
-                    "\"#225290371\" -> \"#297927961\";",
-                    "\"#225290371\" -> \"#1891546521\";",
-                    "\"#1891546521\" -> \"#1312884893\";",
-                    "\"#1891546521\" -> \"#849373393\";",
-                    "\"#1891546521\" -> \"#868964689\";",
-                    "\"#225290371\" -> \"#912011468\";",
-                    "\"#912011468\" -> \"#1881129850\";",
-                    "// Node labels",
-                    "\"#297927961\" [label=\"literal(b)\"];",
-                    "\"#868964689\" [label=\"literal(a)\"];",
-                    "\"#912011468\" [label=\"literal(f)\"];",
-                    "\"#849373393\" [label=\"literal(e)\"];",
-                    "\"#225290371\" [label=\"root\"];",
-                    "\"#1881129850\" [label=\"literal(d)\"];",
-                    "\"#733672688\" [label=\"literal(a)\"];",
-                    "\"#1558021762\" [label=\"root\"];",
-                    "\"#1891546521\" [label=\"literal(c)\"];",
-                    "\"#1312884893\" [label=\"literal(d)\"];",
-                    "}"
-                )
+            ).output(),
+            Matchers.allOf(
+                Matchers.containsString("JsmithGenerativeTree"),
+                Matchers.containsString("literal(a) -> a"),
+                Matchers.containsString("literal(b) -> b"),
+                Matchers.containsString("literal(c) -> literal(d)"),
+                Matchers.containsString("literal(c) -> literal(e)"),
+                Matchers.containsString("literal(c) -> literal(a)"),
+                Matchers.containsString("literal(f) -> literal(d)"),
+                Matchers.containsString("literal(d) -> d")
             )
         );
     }
