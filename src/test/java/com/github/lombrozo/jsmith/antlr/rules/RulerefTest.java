c52c94545ca73acdf4146af10e0798a1627c7ba9
Ilya Shchepetkin <zhao.94@mail.ru>
2025-02-21 00:35:09 +0300

    test: Covered RuleRef class with tests for argActionBlock and elementOptions cases

--- src/test/java/com/github/lombrozo/jsmith/antlr/rules/RulerefTest.java
+++ src/test/java/com/github/lombrozo/jsmith/antlr/rules/RulerefTest.java
@@ -25,6 +25,7 @@ package com.github.lombrozo.jsmith.antlr.rules;
 
 import com.github.lombrozo.jsmith.antlr.Context;
 import com.github.lombrozo.jsmith.antlr.Unparser;
+import java.util.ArrayList;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -48,6 +49,48 @@ final class RulerefTest {
         MatcherAssert.assertThat(
             String.format(
                 "We expect that %s will invoke the linked rule to generate output, but it didn't happen",
+                ruleref
+            ),
+            ruleref.generate(new Context()).text().output(),
+            Matchers.equalTo(expected)
+        );
+    }
+
+    @Test
+    void generatesRuleReferenceByUsingArgActionBlock() throws WrongPathException {
+        final String ref = "reference";
+        final ArgActionBlock actionblock = new ArgActionBlock(new Root(), new ArrayList<>(0));
+        final String expected = "Linked rule value(1)";
+        final String expectedtwo = "Linked rule value(2)";
+        actionblock.append(new Literal(expected));
+        actionblock.append(new Literal(expectedtwo));
+        final Unparser unparser = new Unparser();
+        unparser.with(ref, actionblock);
+        final Ruleref ruleref = new Ruleref(new Empty(), ref, unparser);
+        MatcherAssert.assertThat(
+            String.format(
+                "We expect that %s will invoke the linked argActionBlock to generate output",
+                ruleref
+            ),
+            ruleref.generate(new Context()).text().output(),
+            Matchers.equalTo(expected.concat(expectedtwo))
+        );
+    }
+
+    @Test
+    void generatesRuleReferenceByUsingElementOptions() throws WrongPathException {
+        final String ref = "reference";
+        final ElementOptions elemopts = new ElementOptions(new Empty());
+        final String expected = "Linked rule value";
+        final ElementOption elemopt = new ElementOption(elemopts);
+        elemopt.append(new Identifier(elemopt, expected));
+        elemopts.append(elemopt);
+        final Unparser unparser = new Unparser();
+        unparser.with(ref, elemopts);
+        final Ruleref ruleref = new Ruleref(new Empty(), ref, unparser);
+        MatcherAssert.assertThat(
+            String.format(
+                "We expect that %s will invoke the linked elementOptions to generate output",
                 ruleref
             ),
             ruleref.generate(new Context()).text().output(),
