b4f4da730a0d772bd0ec05ebb202023b5fc004a2
Ilya <zhao.94@mail.ru>
2025-04-02 20:41:20 +0300

    feat: Implemented OptionValue class

--- src/main/java/com/github/lombrozo/jsmith/antlr/rules/OptionValue.java
+++ src/main/java/com/github/lombrozo/jsmith/antlr/rules/OptionValue.java
@@ -23,6 +23,13 @@
  */
 package com.github.lombrozo.jsmith.antlr.rules;
 
+import com.github.lombrozo.jsmith.antlr.Context;
+import com.github.lombrozo.jsmith.antlr.view.Node;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * OptionValue rule.
  * The ANTLR grammar definition:
@@ -36,13 +43,86 @@ package com.github.lombrozo.jsmith.antlr.rules;
  * }
  * @since 0.1
  */
-public final class OptionValue extends Unimplemented {
+public final class OptionValue implements Rule {
+
+    /**
+     * Parent rule.
+     */
+    private final Rule top;
+
+    /**
+     * Elements.
+     */
+    private final List<Rule> elements;
+
     /**
      * Constructor.
      * @param parent Parent rule.
      */
     public OptionValue(final Rule parent) {
-        super(parent);
+        this(parent, new ArrayList<Rule>(0));
+    }
+
+    /**
+     * Constructor.
+     * @param parent Parent rule.
+     * @param elements Elements.
+     */
+    public OptionValue(final Rule parent, final List<Rule> elements) {
+        this.top = parent;
+        this.elements = elements;
+    }
+
+    /**
+     * Constructor.
+     * @param parent Parent rule.
+     * @param rule Element rule.
+     */
+    public OptionValue(final Rule parent, final Rule rule) {
+        this(parent, Stream.of(rule).collect(Collectors.toList()));
+    }
+
+    /**
+     * Constructor.
+     * @param parent Parent rule.
+     * @param literal String literal.
+     */
+    public OptionValue(final Rule parent, final String literal) {
+        this(parent, new Literal(literal));
+    }
+
+    /**
+     * Constructor.
+     * @param parent Parent rule.
+     * @param num Integer.
+     */
+    public OptionValue(final Rule parent, final Integer num) {
+        this(parent, new Literal(num.toString()));
+    }
+
+    @Override
+    public Rule parent() {
+        return this.top;
+    }
+
+    @Override
+    public Node generate(final Context context) throws WrongPathException {
+        if (this.elements.isEmpty()) {
+            throw new IllegalStateException("OptionValue can't be empty");
+        }
+        return new LeftToRight(this, this.elements).generate(context);
+    }
+
+    // We use append only for chain of identifiers with dots between.
+    // So we could only append identifiers and append dot literals inside method.
+    // If we create empty OptionValue and insert rule after initialisation we do not add
+    // dot literal to list.
+    @Override
+    public void append(final Rule rule) {
+        if (!this.elements.isEmpty()) {
+            this.elements.add(new Literal("."));
+        }
+        this.elements.add(rule);
     }
 
     @Override
@@ -52,6 +132,9 @@ public final class OptionValue extends Unimplemented {
 
     @Override
     public Rule copy() {
-        return new OptionValue(this.parent());
+        return new OptionValue(
+            this.parent(),
+            this.elements.stream().map(Rule::copy).collect(Collectors.toList())
+        );
     }
 }
